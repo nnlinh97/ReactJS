ab86337b28d4c85da5f79de2136ee61f64fb555e
DESKTOP-FG9BE67\Linh <linhnn.weebpal@gmail.com>
2018-08-29 23:10:28 +0700

    life circle

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,8 @@ import TaskForm from './Components/WorkingManagement/TaskForm/TaskForm';
 import Control from './Components/WorkingManagement/Control/Control';
 import TaskList from './Components/WorkingManagement/TaskList/TaskList';
 
+const randomstring = require("randomstring");
+
 class App extends Component {
     constructor(props) {
         super(props);
@@ -10,10 +12,37 @@ class App extends Component {
             tasks: []
         }
     }
-    
+
+
+    componentWillMount() { //khi load lại trang thì hàm này được gọi 1 lần duy nhất
+        if (localStorage && localStorage.getItem('tasks')) {
+            let tasks = JSON.parse(localStorage.getItem('tasks'));
+            this.setState({
+                tasks: tasks
+            });
+            console.log(this.state);
+        }
+    }
+
     onGenerateData = () => {
-        console.log('data clicked');
-        
+        let tasks = [
+            {
+                id: randomstring.generate(),
+                name: "Thiet Ke Giao Dien",
+                status: true
+            },
+            {
+                id: randomstring.generate(),
+                name: "Kien Truc Phan Mem",
+                status: false
+            },
+            {
+                id: randomstring.generate(),
+                name: "Kiem Chung Phan Mem",
+                status: true
+            },
+        ];
+        localStorage.setItem('tasks', JSON.stringify(tasks));
     }
     render() {
         return (
@@ -24,7 +53,7 @@ class App extends Component {
                 </div>
                 <div className="row">
                     <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-                        <TaskForm/>
+                        <TaskForm />
                     </div>
                     <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
                         <button type="button" className="btn btn-primary">
@@ -33,11 +62,11 @@ class App extends Component {
                         <button type="button" className="btn btn-danger" onClick={this.onGenerateData}>
                             <span className="glyphicon glyphicon-plus mr-5" /> Data &nbsp;
                         </button>
-                        <br/>
-                        <Control/>
+                        <br />
+                        <Control />
                         <div className="row mt-15">
                             <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                                <TaskList/>
+                                <TaskList />
                             </div>
                         </div>
                     </div>
